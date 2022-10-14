6ff7d61017a7ae4378dee6200144b5e6406447d3
Anietie Idiong <idionga2@gmail.com>
2022-10-13 18:39:51 -0700

    last advance project

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,20 @@
+
+#include <unistd.h>
+
+/**
+ * main- Serves as the entry point for the program
+ *
+ * Return: By default returns zero if no erros
+ */
+int main(void)
+{
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+
+	 59);
+
+
+
+	return (1);
+}
+
