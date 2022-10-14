564639d715f4dbeef27d8177bc705098cd902388
Anietie Idiong <idionga2@gmail.com>
2022-10-13 17:46:35 -0700

    first c code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
-#include <stdio.h>
-/* Include header */
-/* main is where the code runs*/
+#include <stdio.>
+/**
+ * main- Serves as the entry point for the program
+ *
+ * Return: By default returns zero if no erros
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
 
