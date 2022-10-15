0208c93465fdf59e1af68fefd0d442a335f5122d
Anietie Idiong <idionga2@gmail.com>
2022-10-15 03:58:46 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main- The power house of the code
+ *
+ * Return: Return a - z in lower case.
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		printf("%d", num);
+	}
+	printf("\n");
+	return (0);
+}
