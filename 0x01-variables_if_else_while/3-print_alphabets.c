996b2069abd90449d7fa1abdb3123595c847a6b4
Anietie Idiong <idionga2@gmail.com>
2022-10-14 19:43:25 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main- The power house of the code
+ *
+ * Return: Return a - z in lower case.
+ */
+
+int main(void)
+{
+	char aZ;
+
+	for (aZ = 'a'; aZ <= 'z'; aZ++)
+	{
+		putchar(aZ);
+	}
+	for (aZ = 'A'; aZ <= 'Z'; aZ++)
+	{
+		putchar(aZ);
+	}
+	putchar('\n');
+
+	return (0);
+}
