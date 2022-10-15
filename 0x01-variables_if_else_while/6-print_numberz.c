98c617e3f522f9ec0e1c4a031636e5d32db8be47
Anietie Idiong <idionga2@gmail.com>
2022-10-15 04:14:00 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main- The power house of the code
+ *
+ * Return: Return a - z in lower case.
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar((num % 10) + '0');
+	}
+	putchar('\n');
+	return (0);
+}
