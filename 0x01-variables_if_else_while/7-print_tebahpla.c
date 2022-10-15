ad4cf7a1f0b5465d653d129d72b76d2afcfada8d
Anietie Idiong <idionga2@gmail.com>
2022-10-15 05:39:23 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main- The power house of the code
+ *
+ * Return: Return a - z in lower case.
+ */
+
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'z'; alpha >= 'a'; alpha--)
+	{
+		putchar(alpha);
+	}
+	putchar('\n');
+
+	return (0);
+}
