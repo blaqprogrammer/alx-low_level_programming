0ffae4cfaa9e4995cef9a814535edad9795bc70c
Anietie Idiong <idionga2@gmail.com>
2022-10-17 08:09:41 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,17 +9,18 @@
 int main(void)
 {
 	int num;
-	int two;
-	
-	for (num = 0; num < 10; num++)
+	int num1;
+
+	for (num = 0; num < 9; num++)
 	{
-		putchar(num + '0');
-		if (num < 9)
-	{	
-		putchar(',');
-		putchar(' ');
-	}
+		for (num1 = 1; num1 < 10; num1++)
+		{
+			putchar(num + '0');
+			putchar(num1 + '0');
+			putchar(',');
+			putchar(' ');
 		}
+	}
 	putchar('\n');
 
 	return (0);
---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main- The power house of the code
+ *
+ * Return: Return a - z in lower case.
+ */
+
+int main(void)
+{
+	int num;
+	int num1;
+	int num2;
+
+	for (num = 0; num < 8; num++)
+	{
+		for (num1 = 1; num1 < 9; num1++)
+		{
+			for (num2 = 2; num2 < 10; num2++)
+			{
+				if (num != num1 && num != num2)
+				{
+					if (num1 != num2)
+					{
+			putchar(num + '0');
+			putchar(num1 + '0');
+			putchar(num2 + '0');
+			putchar(',');
+			putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
