6f57998704d3b56a5f4d88e0c41c36759bed2d32
Anietie Idiong <idionga2@gmail.com>
2022-10-19 03:23:32 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_alphabet is not returning any value
+ */
+
+void print_alphabet(void)
+{
+	char az;
+	for (az = 'a'; az <= 'z'; az++)
+		_putchar(az);
+		_putchar('\n');
+}
