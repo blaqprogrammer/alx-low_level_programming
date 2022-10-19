1363776c5a3f068361371f00ac0705fd0b25275d
Anietie Idiong <idionga2@gmail.com>
2022-10-19 08:49:45 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isalpha- Entry point
+ *
+ *@c: is the char representing the lowercase.
+ * Return: 1 if char is lowercase, otherwise 0
+ */
+
+
+
+
+int _isalpha(int c)
+{
+	int b;
+
+	if (b >= 'A' && b <= 'Z')
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
