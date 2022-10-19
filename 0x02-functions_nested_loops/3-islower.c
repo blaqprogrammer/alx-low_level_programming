8603cd094f30d89c2caf24d4f4c6067a51c6f5c3
Anietie Idiong <idionga2@gmail.com>
2022-10-19 06:05:46 -0700

    int _islower(int c)

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10- runs the code
+ *
+ * Return: alphabets from a - z.
+ */
+
+int _islower(int c)
+{
+	for (c = 'a'; c <= 'z';)
+		if (c == c)
+	{
+		_putchar(1);
+	}
+	_putchar('\n');
+}
