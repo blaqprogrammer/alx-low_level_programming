050ad7699f62bc26551595cbf3beda2cbdd31797
Anietie Idiong <idionga2@gmail.com>
2022-10-20 12:56:01 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _isupper - this is the entrt point.
+ *@c: integer that carries the command
+ * Return: Returns 1 if caps and 0 if not caps.
+ */
+
+
+
+
+int _isupper(int c)
+{
+	if (c == 'C')
+	{
+		return (0);
+	}
+
+	else
+	{
+		return (0);
+	}
+}
