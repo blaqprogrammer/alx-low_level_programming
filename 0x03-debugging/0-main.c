75e4d1b65b6df6babb8022799fff00d3185daa03
Anietie Idiong <idionga2@gmail.com>
2022-10-20 06:56:49 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,15 +3,16 @@
 
 
 /**
- *positive_or_negative:-This is the entry point.
- *@i: The is the integer that stores the number
+ *main- This is the entry point.
  * Return: 0 if positive or negative
  **/
 
 
 
-int positive_or_negative(int i)
+int main(void)
 {
+	int i;
+
 	i = 0;
 	positive_or_negative(i);
 	return (0);
