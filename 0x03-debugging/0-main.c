71ff3ddc22fb5618b51979cb60c2eadc9d447570
Anietie Idiong <idionga2@gmail.com>
2022-10-20 05:00:45 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,16 +3,16 @@
 
 
 /**
- *positive_or_negative: tests function that prints if integer is positive
- *main- entry point
+ *positive_or_negative:-This is the entry point.
+ *@i: The is the integer that stores the number
  * Return: 0 if positive or negative
- * */
+ */
 
 
 
 int positive_or_negative(int i)
 {
-   	i = 98;
-	        positive_or_negative(i);
-	        return (0);
+	i = 98;
+	positive_or_negative(i);
+	return (0);
 }
