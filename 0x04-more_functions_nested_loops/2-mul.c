5a2d3d23f744cf6cfd8f0b5036c5dfe752c6fa8c
Anietie Idiong <idionga2@gmail.com>
2022-10-20 13:30:25 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * mul - entry point
+ * @a: first integer
+ * @b: second integer
+ * Return: value of c
+ */
+
+
+
+
+int mul(int a, int b)
+{
+	int c;
+
+	for (c = a * b)
+	_putchar(c);
+}
