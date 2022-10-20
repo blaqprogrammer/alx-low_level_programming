734cd8f0261194a6179cd413c9537304a2ec48a6
Anietie Idiong <idionga2@gmail.com>
2022-10-20 04:52:48 -0700

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+
+
+/**
+ *positive_or_negative: tests function that prints if integer is positive
+ *main- entry point
+ * Return: 0 if positive or negative
+ * */
+
+
+
+int positive_or_negative(int i)
+{
+   	i = 98;
+	        positive_or_negative(i);
+	        return (0);
+}
