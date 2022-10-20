dbca9274ac6052c164e8f708f06d5da34ee34c1f
Anietie Idiong <idionga2@gmail.com>
2022-10-20 13:59:43 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_most_numbers - entry point
+ *
+ * Return: 1 - 9 skips 2 and 4
+ */
+
+
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i == 2 && 1 == 4)
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
