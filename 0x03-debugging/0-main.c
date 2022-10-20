f6b911e3f4bafb8fb31eff85701b7f046c69603d
Anietie Idiong <idionga2@gmail.com>
2022-10-20 08:46:16 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,9 +1,14 @@
 #include "main.h"
 
+
+
 /**
- *main - This is the entry point.
- *Return: 0 if positive or negative
+ * main - tests function that prints if integer is positive or negative
+ *
+ * Return: 0
+ *
  */
+
 
 
 int main(void)
@@ -11,9 +16,10 @@ int main(void)
 {
 	int i;
 
-	i = 98 % 2;
+	i = 0;
 	positive_or_negative(i);
 
 
 	return (0);
+
 }
