49364a3b87bcc5c21c3c6156a2cea01a5943ffcf
Anietie Idiong <idionga2@gmail.com>
2022-10-21 16:00:47 -0700

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+
+int main (void)
+{
+	int n;
+
+	for (n = 1; n <= 7; n++)
+	{
+		putchar(char* "_") * n;
+	putchar('\n');
+	return (0);
+	}
+}
+
---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - The entry point of the program
+ *
+ * Return: numbers if % by 3 or 5.
+ */
+
+int main (void)
+
+{
+	int i;
+	
+	for (i = 0; i < 101; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
