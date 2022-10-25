822d54f98d8deb8599b5d38d5bb66bfd07134f77
Anietie Idiong <idionga2@gmail.com>
2022-10-25 08:27:31 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_strlen - The entry point
+ *@s: The main int
+ *Return: Returns the lenght of a string.
+ */
+
+int _strlen(char *s)
+{
+	size_t len = 0;
+
+	while (*str++)
+		len++;
+	return (len);
+}
