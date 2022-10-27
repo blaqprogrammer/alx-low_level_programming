3b7e601e2b28eb423b13369473045ecf9faf69d4
Anietie Idiong <idionga2@gmail.com>
2022-10-27 09:33:37 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strcat - entry mode
+ * @dest: first integer
+ * @src: second integer
+ * Return: a string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	_strcat("%s\n", dest, src);
+	return (0);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is another program of pointers and arrays
