85d3040751b6d59b38197bca73954f1a0ca5896c
Anietie Idiong <idionga2@gmail.com>
2022-10-27 14:30:27 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,7 +9,18 @@
 
 char *_strcat(char *dest, char *src)
 {
-	*_strcat(dest, src);
-	_putchar("%s", dest);
+	int can = 0, can1 = 0;
+	while (*(dest + can)!='\0')
+	{
+		can++;
+	}
+	while (can1 >= 0)
+	{
+		*(dest + can) = *(src + can1);
+		if (*(src + can1) == '\0')
+			break;
+		can++;
+		can1++;
+	}
 	return (dest);
 }
